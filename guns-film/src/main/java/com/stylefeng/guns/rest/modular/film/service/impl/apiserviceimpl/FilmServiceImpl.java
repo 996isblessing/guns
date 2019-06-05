5eca87e1f535ff39544f03460d3159c0c26e9768
wsk <watermelon_1994@163.com>
2019-06-05 09:39:07 +0800

    修改

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/impl/apiserviceimpl/FilmServiceImpl.java
@@ -0,0 +1,6 @@
+package com.stylefeng.guns.rest.modular.film.service.impl.apiserviceimpl;
+
+import com.stylefeng.guns.rest.film.FilmService;
+
+public class FilmServiceImpl implements FilmService {
+}
