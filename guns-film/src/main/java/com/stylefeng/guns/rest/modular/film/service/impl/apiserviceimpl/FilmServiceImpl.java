fd705d9f4250d2a5aa5e516939a2c3d87e33319d
wsk <watermelon_1994@163.com>
2019-06-06 16:03:33 +0800

    修改

--- guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/impl/apiserviceimpl/FilmServiceImpl.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/impl/apiserviceimpl/FilmServiceImpl.java
@@ -2,14 +2,15 @@ package com.stylefeng.guns.rest.modular.film.service.impl.apiserviceimpl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.mysql.cj.x.protobuf.MysqlxCrud;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.stylefeng.guns.rest.common.persistence.model.MtimeFilmT;
 import com.stylefeng.guns.rest.film.FilmService;
 import com.stylefeng.guns.rest.modular.film.service.IMtimeFilmTService;
-import com.stylefeng.guns.rest.modular.film.service.impl.MtimeFilmTServiceImpl;
 import com.stylefeng.guns.rest.persistence.filmmodel.index.IndexResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Service(interfaceClass = FilmService.class)
@@ -21,9 +22,9 @@ public class FilmServiceImpl implements FilmService {
     @Override
     public IndexResult queryIndexMsg() {
         int i = filmTService.selectCount(null);
-
-
-
+        EntityWrapper<MtimeFilmT> wrapper = new EntityWrapper<>();
+        Wrapper<MtimeFilmT> filmTWrapper = wrapper.setSqlSelect("UUID", "film_name", "film_type", "img_address", "film_score");
+        List<MtimeFilmT> filmTS = filmTService.selectList(filmTWrapper);
 
 
         return null;
